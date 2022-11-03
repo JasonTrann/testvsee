c1ffd0c9b7aec863824f6af3cba7950a326b653a
Jason Bond <71129226+JasonTrann@users.noreply.github.com>
2022-11-03 20:04:17 +0700

    Delete main.py

--- partTwo/main.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-response = requests.get("https://api.github.com/search/issues?q=repo:SeleniumHQ/selenium/issues+type:issue+state:open")
-response.json()
-
-open_issue = response.json().get("total_count")
-
-print(open_issue)
